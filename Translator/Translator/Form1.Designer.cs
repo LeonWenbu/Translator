7ead4c854b83370e7ee1e9c53a8cb2bddc29111c
leon <leonhuo@outlook.com>
2017-09-19 08:29:03 +0800

    Init

---
+++ Translator/Translator/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿using System.Windows.Forms;
+using System.Drawing;
+
+
+namespace Translator
+{
+    partial class Form1
+    {
+
+        private System.Windows.Forms.Label label1;
+        private Button btn_right;
+        private Button btn_wrong;
+        private Button btn_refresh;
+        private Label lbl_left;
+
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager componentResourceManager = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new Label();
+            this.btn_right = new Button();
+            this.btn_wrong = new Button();
+            this.btn_refresh = new Button();
+            this.lbl_left = new Label();
+            this.SuspendLayout();
+            this.label1.Font = new Font("宋体", 48f, FontStyle.Regular, GraphicsUnit.Point, (byte)134);
+            this.label1.Location = new Point(200, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new Size(600, 200);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "START";
+            this.label1.TextAlign = ContentAlignment.MiddleCenter;
+            this.btn_right.Font = new Font("宋体", 12f, FontStyle.Bold, GraphicsUnit.Point, (byte)134);
+            this.btn_right.ForeColor = Color.Lime;
+            this.btn_right.Location = new Point(172, 294);
+            this.btn_right.Name = "btn_right";
+            this.btn_right.Size = new Size(80, 30);
+            this.btn_right.TabIndex = 1;
+            this.btn_right.Text = "√";
+            this.btn_right.UseVisualStyleBackColor = true;
+            this.btn_right.Click += new System.EventHandler(this.btn_right_Click);
+            this.btn_wrong.Font = new Font("宋体", 12f, FontStyle.Bold, GraphicsUnit.Point, (byte)134);
+            this.btn_wrong.ForeColor = Color.Red;
+            this.btn_wrong.Location = new Point(737, 294);
+            this.btn_wrong.Name = "btn_wrong";
+            this.btn_wrong.Size = new Size(80, 30);
+            this.btn_wrong.TabIndex = 2;
+            this.btn_wrong.Text = "×";
+            this.btn_wrong.UseVisualStyleBackColor = true;
+            this.btn_wrong.Click += new System.EventHandler(this.btn_wrong_Click);
+            this.btn_refresh.Font = new System.Drawing.Font("宋体", 12f, FontStyle.Regular, GraphicsUnit.Point, (byte)134);
+            this.btn_refresh.ForeColor = SystemColors.ActiveCaptionText;
+            this.btn_refresh.Location = new Point(892, 12);
+            this.btn_refresh.Name = "btn_refresh";
+            this.btn_refresh.Size = new Size(80, 30);
+            this.btn_refresh.TabIndex = 3;
+            this.btn_refresh.Text = "Restart";
+            this.btn_refresh.UseVisualStyleBackColor = true;
+            this.btn_refresh.Click += new System.EventHandler(this.btn_refresh_Click);
+            this.lbl_left.AutoSize = true;
+            this.lbl_left.Font = new System.Drawing.Font("宋体", 15.75f, FontStyle.Regular, GraphicsUnit.Point, (byte)134);
+            this.lbl_left.ForeColor = Color.Red;
+            this.lbl_left.Location = new Point(2, 12);
+            this.lbl_left.Name = "lbl_left";
+            this.lbl_left.Size = new Size(0, 21);
+            this.lbl_left.TabIndex = 4;
+            this.AutoScaleDimensions = new SizeF(6f, 12f);
+            this.AutoScaleMode = AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 462);
+            this.Controls.Add((Control)this.lbl_left);
+            this.Controls.Add((Control)this.btn_refresh);
+            this.Controls.Add((Control)this.btn_wrong);
+            this.Controls.Add((Control)this.btn_right);
+            this.Controls.Add((Control)this.label1);
+            this.Icon = (System.Drawing.Icon)componentResourceManager.GetObject("$this.Icon");
+            this.MaximizeBox = false;
+            this.Name = nameof(Form1);
+            this.Text = "背单词";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+
+    }
+}
+
---
+++ Translator/Translator/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Translator
+{
+    public partial class Form1 : Form
+    {
+        private string path = System.IO.Directory.GetCurrentDirectory() + "\\res\\";
+        private System.Data.DataTable dt = new System.Data.DataTable();
+        private System.Data.DataTable dtno = new System.Data.DataTable();
+        private string forgetFile = "";
+
+        public Form1()
+        {
+            this.InitializeComponent();
+            this.dt.Columns.Add("name");
+            this.dtno.Columns.Add("name");
+
+            forgetFile = Path.Combine(path, "forget.txt");
+
+
+            this.init_Data();
+
+
+            if (this.dt.Rows.Count == 0)
+            {
+                int num = (int)MessageBox.Show("没数据！");
+                Environment.Exit(1);
+            }
+            else
+            {
+                this.showWord();
+            }
+        }
+
+        private void init_Data()
+        {
+            StreamReader streamReader = new StreamReader(this.path + "name.txt");
+            string str;
+            while ((str = streamReader.ReadLine()) != null)
+            {
+                DataRow row = this.dt.NewRow();
+                row["name"] = (object)str;
+                this.dt.Rows.Add(row);
+            }
+            this.lbl_left.Text = "还剩下" + this.dt.Rows.Count.ToString() + "个单词";
+            OutputForgetFileSplitter();
+        }
+
+        private void showWord()
+        {
+            if (this.dt.Rows.Count == 0)
+            {
+                this.label1.Text = "END";
+                this.lbl_left.Text = "还剩下0个单词";
+            }
+            else
+            {
+                this.label1.Text = this.dt.Rows[new Random().Next(this.dt.Rows.Count - 1)]["name"].ToString().Trim();
+                this.lbl_left.Text = "还剩下" + this.dt.Rows.Count.ToString() + "个单词";
+            }
+        }
+
+        private void btn_right_Click(object sender, EventArgs e)
+        {
+            if (this.label1.Text == "END")
+                return;
+            DataRow row = this.dtno.NewRow();
+            row["name"] = (object)this.label1.Text;
+            this.dtno.Rows.Add(row);
+            for (int index = 0; index < this.dt.Rows.Count; ++index)
+            {
+                if (this.dt.Rows[index]["name"].ToString().Trim() == this.label1.Text)
+                    this.dt.Rows.RemoveAt(index);
+            }
+            this.showWord();
+        }
+
+        private void btn_wrong_Click(object sender, EventArgs e)
+        {
+            OutputForgetFile(this.label1.Text);
+            this.showWord();
+        }
+
+        private void btn_refresh_Click(object sender, EventArgs e)
+        {
+            this.dt.Rows.Clear();
+            this.init_Data();
+            this.dtno.Rows.Clear();
+            this.label1.Text = "START";
+        }
+
+        private void OutputForgetFileSplitter()
+        {
+            string dateTime = string.Format("====== {0} ======", DateTimeOffset.Now.ToString());
+            OutputForgetFile(dateTime);
+        }
+
+        private void OutputForgetFile(string message)
+        {
+            using (StreamWriter sw = new StreamWriter(forgetFile, true, Encoding.UTF8))
+            {
+                sw.WriteLine(message);
+            }
+        }
+    }
+}
